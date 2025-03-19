874f626aec41b8e4becf0a665fd167040ecfd4f1
Sergio Ravera <sergioravera@hotmail.de>
2025-03-19 13:50:03 +0100

    displays content if correct html file was requested with GET

--- http_server.sh
+++ http_server.sh
@@ -3,13 +3,18 @@
 function server () {
   while true
   do
-    read message path version
+    read method path version
     
     if [[ $method = "GET" ]]
     then 
-      echo "HTTP/1.1 200 OK"
+      if [[ -f "./www/$path" ]]
+      then 
+        echo -ne "HTTP/1.1 200 OK\r\n\r\n"; cat "./www/$path"
+      else
+        echo -ne "HTTP/1.1 404 Not Found\r\n\r\n"
+      fi
     else
-      echo "HTTP/1.1 400 Bad Request"
+      echo -ne "HTTP/1.1 400 Bad Request\r\n\r\n"
     fi
   done
 }
