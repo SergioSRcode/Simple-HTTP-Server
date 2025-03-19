7e8ab516f40588e54d4322faced76df8c1613bb5
Sergio Ravera <sergioravera@hotmail.de>
2025-03-19 15:12:23 +0100

    replaced while loop with read -a flag

--- http_server.sh
+++ http_server.sh
@@ -3,22 +3,23 @@
 function server () {
   while true
   do
-    read method path version
-    
-    if [[ $method = "GET" ]]
-    then 
+    read -a message_arr
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    if [[ $method = 'GET' ]]
+    then
       if [[ -f "./www/$path" ]]
-      then 
-        echo -ne "HTTP/1.1 200 OK\r\n\r\n"; cat "./www/$path"
+      then
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)\r\n\r\n"; cat "./www/$path"
       else
-        echo -ne "HTTP/1.1 404 Not Found\r\n\r\n"
+        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
       fi
     else
-      echo -ne "HTTP/1.1 400 Bad Request\r\n\r\n"
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
     fi
   done
 }
 
 coproc SERVER_PROCESS { server; }
 
-nc -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+nc -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
